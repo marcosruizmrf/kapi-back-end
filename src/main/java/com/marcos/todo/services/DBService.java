8379219fd91c5bc90b42fa584394866a0965703b
Marcos Ruiz <marcosruizmrf@outlook.com>
2022-06-17 15:27:25 -0300

    fix: Update dbservice

--- src/main/java/com/marcos/todo/services/DBService.java
+++ src/main/java/com/marcos/todo/services/DBService.java
@@ -19,9 +19,9 @@ public class DBService {
 	public void instanciaBaseDeDados() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		Todo t1 = new Todo(1, "Marcos Ruiz", "Arruela Especial", 123, 999, "Em desenvolvimento", "001", sdf.parse("25/03/2022"), false);
+		Todo t1 = new Todo(1, "João", "Arruela Especial", 123, 999, "Em desenvolvimento", "001", sdf.parse("25/03/2022"), false);
 
-		Todo t2 = new Todo(2, "Giulianna Fernandes", "Biela Simétrica", 1234, 1000, "Em desenvolvimento", "002", sdf.parse("22/03/2021"), false);
+		Todo t2 = new Todo(2, "Maria", "Biela Simétrica", 1234, 1000, "Em desenvolvimento", "002", sdf.parse("22/03/2021"), false);
 
 		todoRepository.saveAll(Arrays.asList(t1, t2));
 	}
